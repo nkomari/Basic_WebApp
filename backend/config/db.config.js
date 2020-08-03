cedda2b590a5132c42a5c71369eecdf1a1e23dcc
Nikhil Komari <55295181+nkomari@users.noreply.github.com>
2020-08-03 00:49:47 -0400

    Edited URL string to hide password details

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // url: "mongodb://localhost:27017/master_db",
   url:
-    "mongodb+srv://nkomari:nkhl1995@cluster0.tcqoi.mongodb.net/master_db?retryWrites=true&w=majority",
+    "mongodb+srv://nkomari:<password>@cluster0.tcqoi.mongodb.net/<db_name>?retryWrites=true&w=majority",
 };
